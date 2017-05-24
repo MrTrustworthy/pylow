dfc15dfbcb0c39dc90d3380ef4ad6394c6df6444
Felix <tinywritingmakesmailhardtoread@gmail.com>
2017-05-25 00:58:51 +0200

    improvements to graphics

--- pylow/bokeh_plotter.py
+++ pylow/bokeh_plotter.py
@@ -11,9 +11,9 @@ from bokeh.models import (BasicTicker, CategoricalAxis, CategoricalTicker,
                           ColumnDataSource, DataRange1d, FactorRange, Grid,
                           HoverTool, Line, LinearAxis, Plot, Range1d,
                           SingleIntervalTicker)
-from bokeh.models.glyphs import Circle, Text, VBar, Line, Text
+from bokeh.models.annotations import Label, Title
+from bokeh.models.glyphs import Circle, Glyph, Line, Text, VBar
 from bokeh.sampledata.sprint import sprint
-from bokeh.models.annotations import Title, Label
 
 from .aggregator import Aggregator
 from .datasource import Datasource
@@ -58,27 +58,25 @@ class BokehPlotter:
             'toolbar_location': None,
             'x_range': x_r,
             'y_range': y_r,
+            'min_border': 0,
+            'min_border_left': 0 if self.aggregator.is_in_first_column(plot_info) else 0
         }
         if self.aggregator.is_in_first_row(plot_info):
             # TODO multiple titles for multiple layers of dimensions
             options['title'] = Title(text=plot_info.x_seps[-1].val, align='center')
 
         plot = Plot(**options)
+        # plot.xgrid.grid_line_color = 'navy'
 
         # Labels on the left
         if self.aggregator.is_in_first_column(plot_info):
-            label = Label(x=0, y=options['plot_height']//2, x_units='screen', y_units='screen', text=plot_info.y_seps[-1].val, render_mode='css', text_align='right')
-
+            text = plot_info.y_seps[-1].val.replace(' ', '\n')  # FIXME inserting \n does nothing
+            label = Label(x=0, y=options['plot_height'] // 2, x_units='screen',
+                          y_units='screen', text=text, render_mode='css', text_align='right')
             plot.add_layout(label)
 
         # AXES
-        if self.aggregator.is_in_last_row(plot_info):
-            x_ax = self.get_axis(plot_info, 'x')
-            plot.add_layout(x_ax, 'below')
-
-        if self.aggregator.is_in_first_column(plot_info):
-            y_ax = self.get_axis(plot_info, 'y')
-            plot.add_layout(y_ax, 'left')
+        self.make_axes_and_grids(plot, plot_info)
 
         # GLYPH
         glyph = self.create_glyph(x_colname, y_colname)
@@ -89,8 +87,27 @@ class BokehPlotter:
         plot.add_tools(hover)
         return plot
 
-    def create_glyph(self, x_colname, y_colname):
-        if 'some_condition':
+    def make_axes_and_grids(self, plot, plot_info: PlotInfo) -> None:
+        x_tick, x_ax = self.get_axis(plot_info, 'x')
+        if self.aggregator.is_in_last_row(plot_info):
+            plot.add_layout(x_ax, 'below')
+
+        y_tick, y_ax = self.get_axis(plot_info, 'y')
+        if self.aggregator.is_in_first_column(plot_info):
+            plot.add_layout(y_ax, 'left')
+
+        value = [avp.val for avp in plot_info.x_coords][0]
+        if not isinstance(value, str):
+            grid = Grid(dimension=0, ticker=x_tick, grid_line_dash='dotted')
+            plot.add_layout(grid)
+
+        value = [avp.val for avp in plot_info.y_coords][0]
+        if not isinstance(value, str):
+            grid = Grid(dimension=1, ticker=y_tick, grid_line_dash='dotted')
+            plot.add_layout(grid)
+
+    def create_glyph(self, x_colname: str, y_colname: str) -> Glyph:
+        if 'some_condition':  # TODO FIXME get options from Attribute (or PlotConfig)
             return Line(x=x_colname, y=y_colname, line_width=2)
         elif not 'some_condition':  # TODO FIXME
             return VBar(x=x_colname, top=y_colname)
@@ -107,16 +124,19 @@ class BokehPlotter:
             return Range1d(_min, _max)
 
     def get_axis(self, data: PlotInfo, axis: str):
+
         values = [avp.val for avp in getattr(data, f'{axis}_coords')]
         label = getattr(data, f'{axis}_coords')[0].attr.col_name
+
         if isinstance(values[0], str):
             ticker = CategoricalTicker()
-            axis = CategoricalAxis(ticker=ticker, axis_label=label)
-            return axis
+            axis = CategoricalAxis(ticker=ticker, axis_label=label,
+                                   major_label_orientation=1.57, major_tick_in=0, major_tick_out=0)
         else:
             ticker = BasicTicker(num_minor_ticks=0)
-            axis = LinearAxis(ticker=ticker, axis_label=label)
-            return axis
+            axis = LinearAxis(ticker=ticker, axis_label=label, major_tick_in=0, major_tick_out=0, )
+
+        return ticker, axis
 
     def get_tooltip(self, renderer, plot_info: PlotInfo) -> HoverTool:
         # TODO FIXME: check if there is a 'level' property
